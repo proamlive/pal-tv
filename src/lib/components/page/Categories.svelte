cc7d852032d209eb1a3e549011ad28cd6b94f3fe
Al Murray <2515630+rogueyoshi@users.noreply.github.com>
2025-04-07 00:04:12 -0400

    imgs

--- src/lib/components/page/Categories.svelte
+++ src/lib/components/page/Categories.svelte
@@ -29,4 +29,9 @@
   >
     Behind the Scenes
   </button>
+  <button
+    class="category-chip bg-gray-800 hover:bg-primary text-white px-4 py-2 rounded-full text-sm font-medium whitespace-nowrap"
+  >
+    TV
+  </button>
 </div>
--- src/lib/components/page/Trending.svelte
+++ src/lib/components/page/Trending.svelte
@@ -126,7 +126,7 @@
     >
       <div class="relative">
         <img
-          src="https://images.unsplash.com/photo-1547082299-de196ea013d6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+          src="{base}/balance.jpeg"
           alt="Video thumbnail"
           class="w-full h-40 object-cover"
         />
